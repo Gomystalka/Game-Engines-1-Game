26ad08d3b07fbef886e5baa4f9694d8d305c0d9e
Gomystalka <48233064+Gomystalka@users.noreply.github.com>
2020-11-06 01:48:22 +0000

    Initial upload for Unity project
    Initial upload but some work is done

---
+++ Bryan Game Assignment I Guess Testing/Assets/GayAttribute.cs
@@ -0,0 +1,7 @@
+﻿using System;
+using UnityEngine;
+
+public class GayAttribute : Attribute
+{
+    public bool RandomValue { get; set; }
+}
---
+++ Bryan Game Assignment I Guess Testing/Assets/SynchronizedVarAttribute.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Reflection;
+[AttributeUsage(AttributeTargets.Field)]
+public class SynchronizedVarAttribute : Attribute
+{
+    public string Target { get; set; }
+    public bool HideTargetInInspector { get; set; }
+}
