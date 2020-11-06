07fbe9c3f76f168a5499a16769e4b893b30d4ce7
Gomystalka <48233064+Gomystalka@users.noreply.github.com>
2020-11-06 01:49:09 +0000

    Revert "Initial upload for Unity project"
    This reverts commit 26ad08d3b07fbef886e5baa4f9694d8d305c0d9e.

--- Bryan Game Assignment I Guess Testing/Assets/SynchronizedVarAttribute.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-using System.Reflection;
-[AttributeUsage(AttributeTargets.Field)]
-public class SynchronizedVarAttribute : Attribute
-{
-    public string Target { get; set; }
-    public bool HideTargetInInspector { get; set; }
-}
